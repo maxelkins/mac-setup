032c5dff6b7b2a296de3033ff0b44769f43a4cf0
Max Elkins <max@maxelkins.me>
2026-03-25 18:29:03 +0000

    fix: ohmzsh unattended

--- scripts/ohmyzsh.sh
+++ scripts/ohmyzsh.sh
@@ -5,7 +5,7 @@ echo "--> Oh My Zsh"
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   echo "    Installing Oh My Zsh..."
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -- --unattended
   echo "    Done."
 else
   echo "    Oh My Zsh already installed, skipping."
